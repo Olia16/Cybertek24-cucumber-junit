4a42f1764f289cf35f6fe60e146676997a62f1d7
olhakobiuk <oliakobjuk0952@gmail.com>
2022-01-10 13:59:34 -0500

    1

---
+++ src/test/java/com/cybertek/tests/VyTrackLoginDDTTest.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VyTrackLoginDDTTest {
+
+    @Before
+    public void setUp(){
+
+    }
+    @After
+    public void tearDown(){
+        //close browser here
+    }
+
+    @Test
+    public void loginDDTTest(){
+     //open Excel file
+     //add page object
+     //loop and read credentials
+     //write the result
+
+
+    }
+}
